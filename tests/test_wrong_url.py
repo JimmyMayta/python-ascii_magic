b57b59707bcf5eb9fbd6700bc14f878734323705
Leandro Barone <web@leandrobarone.com.ar>
2020-11-27 13:12:57 -0300

    Streamlined HTTPError traceback

---
+++ tests/test_wrong_url.py
@@ -0,0 +1,4 @@
+from context import ascii_magic
+
+output = ascii_magic.from_url('https://wow.zamimg.com/uploads/blog/images/20516-afterlives-ardenweald-4k-desktop-wallpapers.jpg')
+ascii_magic.to_terminal(output)
